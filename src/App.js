95f41f9e1a0ba168080a724ad1f2ac2ba8dd1804
rgngrc <ayesreginagrace0808@gmail.com>
2026-02-18 11:18:21 +0800

    Add different components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './component/Button';
+import FunctionalComponent from './component/FunctionalComponent';
+import ClassComponent from './component/ClassComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Button variant="secondary" text="Click Me!" onClick={() => alert('Working!')} />
+                <FunctionalComponent name="Ayes" />
+                <ClassComponent />
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/component/FunctionalComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const FunctionalComponent = ({ name }) => {
+    return <div>Hello, {name}!</div>;
+}
+export default FunctionalComponent;
